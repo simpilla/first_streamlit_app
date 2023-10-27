bb10b16e361b007dc1f2db979eb97d6575c45257
simpilla <149200691+simpilla@users.noreply.github.com>
2023-10-27 16:07:42 -0600

    Update streamlit_app.py
    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit
 streamlit.title (' My Healthy Meals Plan  ') 
 streamlit.header('Breakfast Menu')
- 
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔  Hard-Boiled Free-Range Egg')streamlit.header('Breakfast Menu')
+streamlit.text('🥑 🍞 Hard-Boiled Free-Range Egg')
+
 streamlit.text(' 🥣 Idli ')
 streamlit.text ( '  🥗 Dosa')
 streamlit.text (' 🐔 Vada' )
