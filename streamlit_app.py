f46a896a2fe5039874acbd18a9a80b80ab870bed
simpilla <149200691+simpilla@users.noreply.github.com>
2023-10-27 16:08:32 -0600

    Update streamlit_app.py
    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Menu')
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔  Hard-Boiled Free-Range Egg')streamlit.header('Breakfast Menu')
+streamlit.text('🐔  Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🍞 Hard-Boiled Free-Range Egg')
 
 streamlit.text(' 🥣 Idli ')
