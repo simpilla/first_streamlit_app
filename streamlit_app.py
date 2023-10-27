ea3599aa09a686ea64ec1717b5b68fbdebc8e30c
simpilla <149200691+simpilla@users.noreply.github.com>
2023-10-27 13:53:18 -0600

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title (' My Parents New Healthy Dinner ') 
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Idli ')
+streamlit.text ('Dosa')
+streamlit.text ('Vada' )
+streamlit.text('puri')
